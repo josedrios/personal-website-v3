629bbc6488b7f0d4bf6ff457d1672ed748fcacd4
Jose Rios <joserios0531@gmail.com>
2026-04-05 10:42:18 -0700

    Project images and made project cards more responsive

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,17 +1,35 @@
 import Header from "@/components/Header";
 import Link from "next/link";
 import type { ProjectType } from "@/lib/data/projects";
+import Image from "next/image";
 
 export function ProjectCard({ project }: { project: ProjectType }) {
   return (
-    <Link href={"/"} className="project-card">
+    <Link
+      href={project.link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="project-card"
+    >
       <div className="project-card__header">
         <Header type={2}>{project.title}</Header>
         <span className="project-card__header-suffix">-&gt;</span>
       </div>
-      <div className="project-card__image" />
+      <div className="project-card__image-container">
+        <Image
+          width={0}
+          height={0}
+          sizes="100vw"
+          src={project.image}
+          className="project-card__image"
+          alt={"This is a screenshot of " + project.title}
+        />
+      </div>
       <Skills skills={project.skills} />
-      <p className="project-card__description">{project.description}</p>
+      <p className="project-card__description">
+        {project.description}{" "}
+        <span className="project-card__fake-link">[Github Link]</span>
+      </p>
     </Link>
   );
 }
--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -4,6 +4,7 @@ export type ProjectType = {
   description: string;
   skills: string[];
   link: string;
+  image: string;
 };
 
 // TODO: Clean up data here / update it
@@ -15,6 +16,7 @@ export const projects: ProjectType[] = [
     description:
       "This gamer cred website will help players rate games from scores derived from the community. They can also add these gamer cred scores to their own account to accumulate a higher gamer cred score attached to their name. This project is a work in progress.",
     date: "July 2025",
+    image: "/images/projects/gamer_cred.webp",
   },
   {
     title: "Silver Minded",
@@ -31,6 +33,7 @@ export const projects: ProjectType[] = [
     date: "March 2025",
     description:
       "Silver Minded is a desktop application I created in order to place all the wandering thoughts and ideas I had roaming through my head. I find myself spreading all my writings and projects on different pieces of media and platforms as well and I felt like I needed to fix this and centralize all that I need into one piece of software. This application tracks my finances, projects, thoughts and much more. In a nutshell, it contains a calendar for keeping track of events, a finance tracker that also gives me more insight on my spending and saving, and most importantly a network of documents. I can create and index through documents in such a frictionless way that this software serves as the perfect second mind.",
+    image: "/images/projects/silver_minded.webp",
   },
   {
     title: "Clashvote",
@@ -47,12 +50,14 @@ export const projects: ProjectType[] = [
     date: "October 2025",
     description:
       "Clashvote is a voting website where users vote on various aspects on the popular mobile game Clash of Clans, helping players of any skill level. Users also have the ability to search for their own player stats, giving them insight on what their strength and weaknesses are as well as providing them a better overview on both their bases and their respective armies.",
+    image: "/images/projects/clashvote.webp",
   },
   {
     title: "ReadAI",
     link: "https://github.com/josedrios/AI-Book-Bot",
     skills: ["Swift", "Git", "Xcode"],
     date: "December 2024",
+    image: "/images/projects/readai.webp",
     description:
       "With my strong growing interest of reading, I decided to make an app that lets you search for information on a book or get recommendations for books on certain genres utilizing AI. ReadAI takes and displays the information of a given ISBN or a random generated ISBN given a genre and displays rudimentary information on the book but most importantly gives the user an AI generated summarization of the book.",
   },
@@ -63,13 +68,15 @@ export const projects: ProjectType[] = [
     date: "August 2023",
     description:
       "As both a gamer and a programmer, I naturally brought myself to create a game. The game features two Space Tanks, with the ability to shoot, traverse amongst the map, be able to pick up abilities as well as play alongside your friends.",
+    image: "/images/projects/tank_game.webp",
   },
   {
-    title: "Fashion Model Database System",
+    title: "Fashion Database ",
     link: "https://github.com/josedrios/fashion-database-system",
     skills: ["MySQL", "Python", "Git", "IntelliJ"],
     date: "August 2023",
     description:
       "I created a database system with MySQL for the fashion model industry. The process involved laying out the requirements of the system, like what needs to be stored and taken into consideration, creating EER diagrams, implementing it in SQL and even running a dummy discord bot to test and refine the system.",
+    image: "/images/projects/fashion_model.webp",
   },
 ];
