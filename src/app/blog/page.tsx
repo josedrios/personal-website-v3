2e4ad3eb1b613bd614cceac694bf953787c1d414
Jose Rios <joserios0531@gmail.com>
2026-04-27 12:29:57 -0700

    added language to show on code snippet, styled overall code component

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,6 +3,7 @@ import BlogCard from "@/components/BlogCard";
 import Header from "@/components/Header";
 import { Metadata } from "next";
 import Note from "@/components/Note";
+import Code from "@/components/Code";
 
 export const metadata: Metadata = {
   title: "Blog",
@@ -10,10 +11,14 @@ export const metadata: Metadata = {
 };
 
 export default function Blog() {
+  const temp = `name = 'jose'
+for i in range(len(jose)):
+    print(i) # taskjbsdkgsnmdgsfgnmdfg`;
   const blogs = getSortedPostsData();
   return (
     <main>
       <Header type={1}>My Blog</Header>
+      {/* <Code text={temp} language="python" /> */}
       <Note title="disclaimer">
         This is a very unserious, all over the place, typo infested blog page. I
         just want to be able to write better and share my thoughts.
--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from "react";
 
 import Prism from "prismjs";
+// Prismjs theme
 import "automad-prism-themes/dist/prism-automad-dark.css";
 // Import all languages that will be used through this code snippet component
 import "prismjs/components/prism-typescript";
@@ -10,14 +11,8 @@ import "prismjs/components/prism-python";
 // Import for line numbers
 import "prismjs/plugins/line-numbers/prism-line-numbers.js";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
-// Import for tool bar (shows language on hover) (can have copyable button on toolbar as well)
-import "prismjs/plugins/toolbar/prism-toolbar.js";
-import "prismjs/plugins/toolbar/prism-toolbar.css";
-import "prismjs/plugins/show-language/prism-show-language.js";
 
-// TODO: make langauge always show (not only on hover)
-// TODO: add copy code button
-// TODO: change highlight color
+// CURRENT: add copy code button
 
 export default function Code({
   text,
@@ -31,8 +26,12 @@ export default function Code({
   }, [text, language]);
 
   return (
-    <pre className="code-block">
-      <code className={`language-${language} line-numbers`}>{text}</code>
-    </pre>
+    <div className="code-block-container">
+      <pre className="code-block">
+        <code className={`language-${language} line-numbers`}>{text}</code>
+      </pre>
+      {/* add copy button that shows on hover (absolute - top right - make sure to add min height for code block so coyp and language dont over lap */}
+      <p className="code-block-language">{language.toUpperCase()}</p>
+    </div>
   );
 }
